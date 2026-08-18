638d9965a40e14f0803bb8d196df9a66a2c70fcc
Empire6565 <162064635+Empire6565@users.noreply.github.com>
2026-08-18 11:13:55 +0330

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Server is starting..."
+# در اینجا در آینده کدهای دانلود هسته پروکسی یا اجرای پایتون قرار می‌گیرد
+# برای تست، یک وب‌سرویس ساده روی پورت 7860 روشن نگه می‌داریم:
+while true; do echo -e "HTTP/1.1 200 OK\n\n Cloud Proxy Server
